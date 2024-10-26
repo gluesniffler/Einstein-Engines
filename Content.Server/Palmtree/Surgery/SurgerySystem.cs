3e66ce949949c250cf9feea909bb78dea38acb40
gluesniffler <linebarrelerenthusiast@gmail.com>
2024-10-26 18:36:27 -0400

    Revert "Merge pull request #16 from Tropica1Owl/Pogmed"
    This reverts commit 86a829e542a51a361378d13e4f2e24b6e92860b6, reversing
    changes made to 2747975c71ce698189ca5190e62b52779d4f147e.

--- Content.Server/Palmtree/Surgery/SurgerySystem.cs
+++ Content.Server/Palmtree/Surgery/SurgerySystem.cs
@@ -4,7 +4,6 @@ using Content.Server.Palmtree.Surgery;
 using Content.Server.Body.Systems;
 using Content.Server.Popups;
 using Content.Server.Mind;
-using Content.Shared.Inventory;
 using Content.Shared.Atmos.Rotting;
 using Content.Shared.Palmtree.Surgery;
 using Content.Shared.Interaction;
@@ -64,7 +63,7 @@ namespace Content.Server.Palmtree.Surgery.SurgerySystem
         [Dependency] private readonly SharedMindSystem _sharedmind = default!;
         [Dependency] private readonly MindSystem _mind = default!;
         [Dependency] private readonly StandingStateSystem _standing = default!;
-        [Dependency] private readonly InventorySystem _inventory = default!;
+        //[Dependency] private readonly IPlayerManager _player = default!;
         [Dependency] private readonly SharedRottingSystem _rot = default!;
         [Dependency] private readonly BloodstreamSystem _blood = default!;
         [Dependency] private readonly IGameTiming _timing = default!;
@@ -255,8 +254,6 @@ namespace Content.Server.Palmtree.Surgery.SurgerySystem
         }
         private void OnAfterInteract(EntityUid uid, PSurgeryToolComponent tool, AfterInteractEvent args) // Turn this into FTL strings later
         {
-            int surgeryTimeMultiplier = 1;
-            bool messageShown = false; // Avoid pop up stacking
             bool patientHasState = TryComp(args.Target, out MobStateComponent? patientState);
             if (!args.CanReach || args.Target == null) // We already check if target is null, it's okay to perform direct conversion to non-nullable
             {
@@ -264,45 +261,29 @@ namespace Content.Server.Palmtree.Surgery.SurgerySystem
             }
             if (!TryComp(args.Target, out PPatientComponent? patient) && patientHasState)
             {
+                _popupSystem.PopupEntity("You cannot perform surgery on this!", args.User, PopupType.Small);
                 return;
-            }
-            if (_inventory.TryGetSlotEntity((EntityUid) args.Target, "outerClothing", out var outer) || _inventory.TryGetSlotEntity((EntityUid) args.Target, "jumpsuit", out var jumpsuit))
-            {
-                _popupSystem.PopupEntity("Remove the patient's clothes first!", args.User, PopupType.MediumCaution);
-                return;
-            }
-            if (args.User == args.Target) // I've reordered the checks so that the most important modifiers are warned about first.
-            {
-                if (!messageShown)
-                {
-                    _popupSystem.PopupEntity("You begin operating yourself, this isn't exactly ideal.", args.User, PopupType.MediumCaution);
-                    messageShown = true;
-                }
-                surgeryTimeMultiplier += 3;
             }
             if (!_standing.IsDown((EntityUid) args.Target))
             {
-                if (!messageShown)
-                {
-                    _popupSystem.PopupEntity("The patient should be laying down!", args.User, PopupType.MediumCaution);
-                    messageShown = true;
-                }
-                surgeryTimeMultiplier += 3;
+                _popupSystem.PopupEntity("The patient must be laying down and asleep!", args.User, PopupType.Small);
+                return;
             }
             if (!TryComp(args.Target, out SleepingComponent? sleep) && !_mobState.IsIncapacitated((EntityUid) args.Target, patientState))
             {
-                if (!messageShown)
-                {
-                    _popupSystem.PopupEntity("You begin the procedure with them awake, this is gonna hurt.", args.User, PopupType.MediumCaution);
-                    messageShown = true;
-                }
-                surgeryTimeMultiplier += 1;
+                _popupSystem.PopupEntity("The patient must be asleep!", args.User, PopupType.Small);
+                return;
             }
-            var doAfterEventArgs = new DoAfterArgs(EntityManager, args.User, tool.useDelay * surgeryTimeMultiplier, new SurgeryDoAfterEvent(), uid, target: args.Target)
+            if (args.User == args.Target)
+            {
+                _popupSystem.PopupEntity("You cannot operate yourself!", args.User, PopupType.Small);
+                return;
+            }
+            var doAfterEventArgs = new DoAfterArgs(EntityManager, args.User, tool.useDelay, new SurgeryDoAfterEvent(), uid, target: args.Target)
             {
                 NeedHand = true,
-                BreakOnUserMove = true,
-                BreakOnTargetMove = true,
+                //BreakOnMove = true,
+                //BreakOnWeightlessMove = true,
             };
             _audio.PlayPvs(tool.audioStart, args.User);
             _doAfter.TryStartDoAfter(doAfterEventArgs);
